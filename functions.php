e9d024670301a23bd4343fe6a1af242dba13aed5
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-01-07 22:09:46 +0100

    newww

--- functions.php
+++ functions.php
@@ -8,3 +8,7 @@ function add_script() {
     wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js', array(), false, true);
   }
   add_action( 'wp_enqueue_scripts', 'add_script' );
+  ?>
+<?php 
+  add_image_size('custom_size', 50, 50, true);
+?>
--- templates/contact.php
+++ templates/contact.php
@@ -4,3 +4,21 @@
 */
 ?>
 CONTACT HTML 
+<br>
+<aside>
+<h2>Email</h2>
+<?php the_field(selector: "email"); ?>
+<hr>
+<h2>Coordonées GPS</h2>
+<?php $coordinates = get_field(selector: "coordinates"); ?> 
+<p>Latitude: <?php echo $coordinates['latitude']; ?></p>
+<p>Longitude: <?php echo $coordinates['longitude']; ?></p>
+<hr>
+<h2>Logo</h2>
+<?php $image = get_field(selector: "image"); ?>
+<img src="<?php echo ($image ['sizes']['custom_size']);?>" height="<?php echo ($image ['sizes']['custom_size-height']);?>" width="<?php echo ($image ['sizes']['custom_size-width']);?>" alt='<?php echo ($image ['alt']);?>'>
+<hr>
+<pre>
+<?php var_dump($image); ?>
+</aside>
+</pre>
