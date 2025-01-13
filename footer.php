a1d5335dc75417c224c78cef11205cfc1b7972d2
iMariee <marie.incandela@epitech.eu>
2025-01-13 16:57:53 +0100

    [ADD] footer

--- footer.php
+++ footer.php
@@ -1,6 +1,52 @@
 <footer>
-    <h2>Je suis un footer</h2>
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
+  <div class="footer-container">
+    <!-- Section Logo -->
+    <div class="footer-logo">
+      <img src="http://wordpress.test/wp-content/uploads/2025/01/Logo.svg" alt="Multi Pattes Logo">
+    </div>
+
+    <!-- Section Informations -->
+    <div class="footer-info">
+      <ul>
+        <li class="legal"><a href="#mentions-legales">Mentions légales</a></li>
+        <li class="reserver"><a href="#comment-reserver">Comment réserver ?</a></li>
+        <li class="services"><a href="#mes-services">Mes services</a></li>
+      </ul>
+    </div>
+
+    <!-- Section Contact -->
+    <div class="footer-contact">
+      <p class="contact">Contact</p>
+      <div class="contact-item">
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/phone.svg" alt="Téléphone">
+        <p class="num">06 87 20 04 32</p>
+      </div>
+      <div class="contact-item">
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/house.svg" alt="Adresse">
+        <p class="adress">Chimilin, France</p>
+      </div>
+    </div>
+
+    <!-- Section Réseaux Sociaux -->
+    <div class="footer-socials">
+      <a href="URL-FACEBOOK" target="_blank"><img src="http://wordpress.test/wp-content/uploads/2025/01/Facebook-Logo.png" alt="Facebook"></a>
+      <a href="URL-TIKTOK" target="_blank"><img src="http://wordpress.test/wp-content/uploads/2025/01/TikTok-Logo.png" alt="TikTok"></a>
+      <a href="URL-INSTAGRAM" target="_blank"><img src="http://wordpress.test/wp-content/uploads/2025/01/Instagram-Logo.png" alt="Instagram"></a>
+    </div>
+  </div>
+
+  <!-- Pattes SVG -->
+  <div class="footer-paws">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+    <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Patte">
+  </div>
 </footer>
-<?php wp_footer(); ?>
-</body>
-</html>
--- header.php
+++ header.php
@@ -4,6 +4,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+  </style>
   <title>Mon site internet</title>
   <?php wp_head(); ?>
 </head>
