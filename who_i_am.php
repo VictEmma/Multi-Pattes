42ee30a76d55ce05ce76f129c1b673f2f1b90c0e
iMariee <marie.incandela@epitech.eu>
2025-01-14 16:30:44 +0100

    [MOD] finished who i am page

--- who_i_am.php
+++ who_i_am.php
@@ -1,55 +1,42 @@
 <?php /* Template Name: Qui suis-je ? */ ?>
 <?php get_header(); ?>
-<!DOCTYPE html>
 <html lang="fr">
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Qui suis-je ?</title>
    <link rel="stylesheet" href="reset.css">
-   <link rel="stylesheet" href="style.css">
    <link rel="stylesheet" href="who_style.css">
 </head>
 <body>
-   <header>
-      <nav>
-         <a href="index.html">Accueil</a> > Qui suis-je ?
-      </nav>
-   </header>
-   <main>
+   <h1>Qui suis-je ?</h1>
       <section class="about">
-         <h1>Qui suis-je ?</h1>
-         <div class="content">
-            <div class="text">
-               <p>
+               <h2 class="intro-text">
                   Je m'appelle <strong>Fabienne Guinet</strong>, et je suis une <strong>passionnée de chiens</strong> depuis toujours.
                   Par le passé, j'ai été <strong>aide soignante</strong> en service gériatrique, puis je suis devenue <strong>assistante vétérinaire</strong> avant de me spécialiser en
                   <strong>physiothérapie et en rééducation canine</strong>.
-               </p>
-               <p>
-                  Ayant des chiens handicapés et âgés, j'ai rencontré beaucoup de difficultés lorsque je devais les laisser à quelqu'un pour s'en occuper.
-                  J'ai donc décidé par la suite de créer mon entreprise car il n'y a pas assez de prises en charge en clinique pour les chiens à pathologies similaires.
-                  Pour ce faire, j'ai obtenu divers diplômes et fait plusieurs formations, pour être la plus qualifiée possible pour répondre aux besoins de ces animaux.
-               </p>
+               </h2>
+               <div class="img-txt">
+                  <p>
+                     Ayant des chiens handicapés et âgés, j'ai rencontré beaucoup de difficultés lorsque je devais les laisser à quelqu'un pour s'en occuper.
+                     J'ai donc décidé par la suite de créer mon entreprise car il n'y a pas assez de prises en charge en clinique pour les chiens à pathologies similaires.
+                     Pour ce faire, j'ai obtenu divers diplômes et fait plusieurs formations, pour être la plus qualifiée possible pour répondre aux besoins de ces animaux.
+                  </p>
+                  <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Carte où me retrouver" width="414" height="426">
+               </div>
+            <div class="title">
+               <h2 class="where-txt">Où me retrouver ?</h2>
             </div>
-            <div class="image">
-               <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Carte où me retrouver" width="414" height="426">
-            </div>
-         </div>
          <div class="additional">
-            <h2>Où me retrouver ?</h2>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22343.25489566852!2d5.567836471909561!3d45.572311540243284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6354a6efc59d41fd%3A0x39d6bc66817d82ca!2sMultipattes!5e0!3m2!1sen!2sfr!4v1736845489528!5m2!1sen!2sfr" width="414" height="420" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-         </div>
-         <div class="side-text">
-            <p>
-               Je souhaite donner l'opportunité aux maîtres de chiens difficilement acceptés dans une pension classique de partir en vacances sereinement
-               en confiant leurs animaux à un organisme qualifié ou encore de permettre aux animaux d'obtenir une amélioration de leur santé.
-               Étant également une grande fan de molosses, je possède le diplôme CESCCAM, et je suis donc en mesure de former quiconque possédant ou
-               souhaitant acheter un chien catégorisé. Au plaisir de vous rencontrer, n'hésitez pas à regarder plus en détails les services que Multi Pattes propose !
-            </p>
+               <p>
+                  Je souhaite donner l'opportunité aux maîtres de chiens difficilement acceptés dans une pension classique de partir en vacances sereinement
+                  en confiant leurs animaux à un organisme qualifié ou encore de permettre aux animaux d'obtenir une amélioration de leur santé.
+                  Étant également une grande fan de molosses, je possède le diplôme CESCCAM, et je suis donc en mesure de former quiconque possédant ou
+                  souhaitant acheter un chien catégorisé. Au plaisir de vous rencontrer, n'hésitez pas à regarder plus en détails les services que Multi Pattes propose !
+               </p>
          </div>
       </section>
-   </main>
 </body>
 </html>
 <?php get_footer(); ?>
