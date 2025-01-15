e0b46233ced5341e4ae3dbd4bf5f9394ea9b8719
iMariee <marie.incandela@epitech.eu>
2025-01-15 10:03:06 +0100

    [ADD] finished services page

--- services.php
+++ services.php
@@ -1,13 +1,50 @@
 <?php /* Template Name: Mes services */ ?>
 <?php get_header(); ?>
 
-
     <h1>Mes services</h1>
     <section class="about">
         <div class="first">
+            <div class="icon-and-text">
             <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Physiothérapie" class="icon">
             <h2>Garde chien pathologique</h2>
-            <h3>Vous êtes en quête d'une personne à qui confier votre chien âgé ou handicapé ? Ne cherchez plus !</h3>
+            </div>
+            <h3>Vous êtes en quête d'une personne à qui confier<br>votre chien âgé ou handicapé ? Ne cherchez plus !</h3>
+            <p class="button">En savoir +</p>
+        </div>
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Rééducation" class="img">
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Rééducation" class="img">
+        <div class="first">
+            <div class="icon-and-text">
+            <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Physiothérapie" class="icon">
+            <h2>Parc de jeux canin</h2>
+            </div>
+            <h3>Un parc avec tout ce qu'il faut pour que votre<br>toutou d'amour s'amuse en sécurité !</h3>
+            <p class="button">En savoir +</p>
+        </div>
+        <div class="first">
+            <div class="icon-and-text">
+            <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Physiothérapie" class="icon">
+            <h2>Formatrice chien catégorisé</h2>
+            </div>
+            <h3>Vous souhaitez obtenir le diplôme vous<br>autorisant à avoir un chien catégorisé ? Bonne<br>nouvelle, je peux vous faire passer la formation !</h3>
+            <p class="button">En savoir +</p>
+        </div>
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Rééducation" class="img">
+        <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Rééducation" class="img">
+        <div class="first">
+            <div class="icon-and-text">
+            <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Physiothérapie" class="icon">
+            <h2>Préparation physique</h2>
+            </div>
+            <h3>Votre toutou a besoin de récupération physique ?<br>Je peux les aider à se remettre en jambes<br>(ou en pattes) !</h3>
+            <p class="button">En savoir +</p>
+        </div>
+        <div class="first">
+            <div class="icon-and-text">
+            <img src="http://wordpress.test/wp-content/uploads/2025/01/Patounne.svg" alt="Physiothérapie" class="icon">
+            <h2>Remplacement poste ASV</h2>
+            </div>
+            <h3>Vous êtes vétérinaire, et vous souhaitez un renfort<br>ou un remplacement ?</h3>
             <p class="button">En savoir +</p>
         </div>
         <img src="http://wordpress.test/wp-content/uploads/2025/01/PaperPreview-Dark-RegularText-RegularColors.png" alt="Rééducation" class="img">
