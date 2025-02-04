00065108b48b412899e5e14e2a771ad7d67c99b1
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-02-04 09:30:30 +0100

    Réglage de tous les anciens soucis ptn

--- les_services.php
+++ les_services.php
@@ -8,21 +8,23 @@
         <?php echo get_field('introduction'); ?> 
     </h2>
     <div class="content">
-    <?php 
-    $blocs = get_field('blocks');
+        <?php 
+        $blocs = get_field('blocks');
 
-    foreach ($blocs as $key => $bloc) {
-        if ($key % 2 == 0) { // Texte avant image
-            echo "<p>{$bloc['text']}</p>";
-            echo "<img src='{$bloc['image']['url']}' alt='Image de présentation'>";
-        } else { // Image avant texte
-            echo "<img src='{$bloc['image']['url']}' alt='Image de présentation'>";
-            echo "<p>{$bloc['text']}</p>";
+        foreach ($blocs as $key => $bloc) {
+            echo '<div class="bloc">';
+            if ($key % 2 == 0) { // Texte avant image
+                echo "<p>{$bloc['text']}</p>";
+                echo "<img src='{$bloc['image']['url']}' alt='Image de présentation'>";
+            } else { // Image avant texte
+                echo "<img src='{$bloc['image']['url']}' alt='Image de présentation'>";
+                echo "<p>{$bloc['text']}</p>";
+            }
+            echo '</div>';
         }
-    }
-    ?>
-</div>
-
+        ?>
+    </div>
+</section>
     <p class="end-text">
     <?php echo get_field('text'); ?>
     </p>
