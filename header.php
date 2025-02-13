f5a013edd0451ff3cdd3a8ad979e1ed45e21fb3b
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-02-13 22:37:05 +0100

    réglage soucis header et footer

--- header.php
+++ header.php
@@ -5,56 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="path/to/your/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 </head>
 <body <?php body_class(); ?>>
-<header>
-    <ul>
-        <li><a href="#">Qui suis-je ?</a></li>
-        <li><a href="#">Mes services</a></li>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Logo_Text.svg" alt="Mon logo"></a>
-        <li><a href="#">Comment réserver ?</a></li>
-        <li><a href="#">Contact</a></li>
-        <a href="#" id="openBtn">
-            <span class="burger-icon">
-                <span></span>
-                <span></span>
-                <span></span>
-            </span>
-        </a>
+
+  <header>
+    <!-- Menu normal (PC) -->
+    <ul class="menu-desktop">
+      <li><a href="">Qui suis-je ?</a></li>
+      <li><a href="">Mes services</a></li>
+      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Logo_Text.svg" alt="Mon logo"></a>
+      <li><a href="">Comment réserver ?</a></li>
+      <li><a href="">Contact</a></li>
     </ul>
-    <div id="mySidenav" class="sidenav">
-        <a id="closeBtn" href="#" class="close">×</a>
-        <ul>
-            <li><a href="#">A propos</a></li>
-            <li><a href="#">Nos services</a></li>
-            <li><a href="#">Comment réserver ?</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+
+    <!-- Menu burger (Mobile) -->
+    <div class="menu-mobile">
+    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Logo_Text.svg" alt="Mon logo"></a>
+      <a href="#" id="openBtn">
+        <span class="burger-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </a>
     </div>
-</header>
-<script>
-    // Sélection des éléments
+
+    <!-- Contenu du menu mobile -->
+    <div id="mySidenav" class="sidenav">
+      <a id="closeBtn" href="#" class="close">×</a>
+      <ul>
+        <li><a href="#">A propos</a></li>
+        <li><a href="#">Nos services</a></li>
+        <li><a href="#">Témoignages</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </header>
+
+  <script>
     var sidenav = document.getElementById("mySidenav");
     var openBtn = document.getElementById("openBtn");
     var closeBtn = document.getElementById("closeBtn");
 
-    // Ouvrir le menu
     openBtn.onclick = function () {
-        openNav();
+      sidenav.classList.add("active");
     };
 
-    // Fermer le menu
     closeBtn.onclick = function () {
-        closeNav();
+      sidenav.classList.remove("active");
     };
-
-    function openNav() {
-        sidenav.classList.add("active"); // Ajoute la classe active
-    }
-
-    function closeNav() {
-        sidenav.classList.remove("active"); // Supprime la classe active
-    }
-</script>
-
+  </script>
