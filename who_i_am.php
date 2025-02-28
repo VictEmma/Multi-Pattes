3984b11311d4702dafa68b267651d9fb730be8e3
VictEmma <victor-emmanuel.ndayishimiye@etu.univ-smb.fr>
2025-02-28 17:55:49 +0100

    petit truc à finir (page qui suis-je)

--- who_i_am.php
+++ who_i_am.php
@@ -1,5 +1,6 @@
 <?php /* Template Name: Qui suis-je ? */ ?>
 <?php get_header(); ?>
+   <title>Qui suis-je ?</title>
    <h1>
       Qui suis-je ?
    </h1>
@@ -8,7 +9,7 @@
                <?php echo get_field('intro_who'); ?>
       </div>
    <div class="img-txt">
-  <div>
+  <div class="text-button">
       <p><?php echo get_field('explain_text'); ?></p>
       <?php 
          $monlien = get_field('truffinoscope');
@@ -36,7 +37,7 @@
                <h2 class="custom-title">Où me retrouver ?</h2>
                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22343.25489566852!2d5.567836471909561!3d45.572311540243284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6354a6efc59d41fd%3A0x39d6bc66817d82ca!2sMultipattes!5e0!3m2!1sen!2sfr!4v1736845489528!5m2!1sen!2sfr" width="414" height="420" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>   
-            <div>
+            <div class="text-button">
                <?php echo get_field('map_text'); ?>
                   <a class="button" href="https://multi-pattes.fr/contacts/">
                      Contactez moi !
